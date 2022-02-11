e28d19387cb3698b26825d242896b83a3e11c0ec
Wojciech Fraczak <fraczak@gmail.com>
2022-02-10 22:39:01 -0500

    Update this.js

--- this.js
+++ this.js
@@ -1,6 +1,6 @@
 var rectangle = {hauteur: 10, largeur:20};
 var surface = rect => rect.hauteur * rect.largeur; 
-// > surface()
+// > surface(rectangle)
 // 200
 
 // Question: rectangle.surface = function(){ ... };
